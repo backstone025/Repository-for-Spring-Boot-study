a82dc9c879dc32a9c853d6fce0ad64152c440ce5
Bea gyeong seok <gsr025silver.naver.com>
2024-08-18 16:10:19 +0900

    실습 : HelloWorldConfiguration(3차) - Bean에 사용자 지정 이름을 설정해 보았다. - 2가지 방법(사용자 지정 이름, Bean의 유형)을 사용해 Bean 호출하기 	- 같은 유형을 가진 Beans의 유형으로 Bean의 유형을 이용해 Bean을 호출할 시 어떤 error가 발생하는지 확인해보았다. - 3가지 방법(method 호출, record 객체, parameter)를 사용해 다른 class를 종속시키지

--- backstone/src/main/java/LearningSpringBoot/backstone/App02_HelloWorldSpring.java
+++ backstone/src/main/java/LearningSpringBoot/backstone/App02_HelloWorldSpring.java
@@ -20,5 +20,24 @@ public class App02_HelloWorldSpring {
         System.out.println(context.getBean("age"));
 
         System.out.println(context.getBean("person"));
+
+        // Bean에 사용자 지정 이름을 붙여주어 호출하기
+        System.out.println(context.getBean("address1"));
+
+        // Bean의 유형을 매개변수로 사용하여 Bean 호출하기
+        System.out.println(context.getBean(Address.class));
+
+        // Bean끼리 종속성을 갖도록 만들기
+        // 1. method 호출
+        System.out.println(context.getBean("person2MethodCall"));
+
+        // 2. record 객체에 종속시키기
+        System.out.println(context.getBean("clone"));
+
+        // 3. parameter로 다른 class로부터 가져와오기
+        System.out.println(context.getBean("person3Parameters"));
+
+        // 문제점 : Bean의 유셩을 매개변수로 사용하여 호출할 시, 만일 같은 유형의 Beans이 여러개 있으면, 여러 개의 Bean이 발견되었다는 예회를 출력한다.
+        System.out.println(context.getBean(Person.class));
     }
 }
--- backstone/src/main/java/LearningSpringBoot/backstone/HelloWorldConfiguration.java
+++ backstone/src/main/java/LearningSpringBoot/backstone/HelloWorldConfiguration.java
@@ -1,9 +1,16 @@
 package LearningSpringBoot.backstone;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {}
+record Person(String name, int age) {
+}
+
+record Clone(String name, int age, Address address){}
+
+record Address(String firstLine, String city) {
+}
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -17,9 +24,32 @@ public class HelloWorldConfiguration {
     public int age() {
         return 23;
     }
+
     @Bean
-    public Person person(){
+    public Person person() {
         return new Person("Ravi", 20);
     }
 
+    // method 호출로 class 상호 간에 종속되도록 하기
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age());
+    }
+
+    // recod 객체에 parameter로 다른 record 객체를 넣어 종속되게 하기
+    @Bean
+    public Clone clone(){
+        return new Clone("Ravi", 20, new Address("Main Street", "Main City"));
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age){
+        return new Person(name, age);
+    }
+
+    // Bean에 사용자 지정 이름을 부여한다.
+    @Bean(name = "address1")
+    public Address address(){
+        return new Address("Baker Street", "London");
+    }
 }
