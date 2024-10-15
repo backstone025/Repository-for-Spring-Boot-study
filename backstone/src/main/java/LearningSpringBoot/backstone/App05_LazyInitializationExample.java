bc36752f8fb4ce8a2b01a45b7abe9701d10baf21
Bea gyeong seok <gsr025silver.naver.com>
2024-10-15 20:52:09 +0900

    실습 : LazyinitializationExample(3차) - @PostConstruct를 이용해, initialization을 위해 의존성 주입을 마친 후, ClassA의 ready 함수를 실행하도록 구현하였다. - @PreDestroy를 이용해, process가 제저되기 전, cleanup 함수의 instance를 callback하도록 구현하였다.

--- backstone/src/main/java/LearningSpringBoot/backstone/App05_LazyInitializationExample.java
+++ backstone/src/main/java/LearningSpringBoot/backstone/App05_LazyInitializationExample.java
@@ -1,34 +1,49 @@
 package LearningSpringBoot.backstone;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 @Component
-class ClassA{
+class ClassA {
+    private ClassB B;
+
+    public ClassA(ClassB B) {
+        super();
+        this.B = B;
+        System.out.println("All dependency is ready.");
+    }
+
+    // @PostConstruct -> 해당 method가 initialization을 위해 의존성 주입을 마친 후 실행하도록 한다.
+    @PostConstruct
+    public void initialize(){
+        B.ready();
+    }
+
+    // @PreDestroy -> container에 의해 process가 삭제되기 전, 해당 instance를 callback한다.
+    @PreDestroy
+    public void cleanup(){
+        System.out.println("clean up.");
+    }
 }
 
 @Component
-@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-class ClassB{
-
+class ClassB {
+    public void ready() {
+        System.out.println("Some logic using");
+    }
 }
 
 @Configuration
 @ComponentScan
 public class App05_LazyInitializationExample {
     public static void main(String[] arg) {
-        try(var context = new AnnotationConfigApplicationContext(App05_LazyInitializationExample.class)){
-            System.out.println(context.getBean(ClassB.class));
-            System.out.println(context.getBean(ClassB.class));
-            System.out.println(context.getBean(ClassB.class));
-
-            System.out.println(context.getBean(ClassA.class));
-            System.out.println(context.getBean(ClassA.class));
-            System.out.println(context.getBean(ClassA.class));
+        try (var context = new AnnotationConfigApplicationContext(App05_LazyInitializationExample.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
         }
     }
 }
