cbd3d1bbbbb861145d3f8427f26fdcbfb72a8bcb
Bea gyeong seok <gsr025silver.naver.com>
2024-10-26 16:14:35 +0900

    실습 : CourseController - tomcat을 사용하여 간단한 rest api를 구현해보았다.

---
+++ learn-spring-boot/src/main/java/com/backstone/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,18 @@
+package com.backstone.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "backstone"), new Course(2, "Learn DevOps", "backstone")
+        );
+    }
+
+}
---
+++ learn-spring-boot/src/main/java/com/backstone/springboot/learn_spring_boot/LearnSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.backstone.springboot.learn_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// http://localhost:8080/courses
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
