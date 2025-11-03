ef2b4b055fa4ebd501e757f465ec0bdd2548a5ee
Bea gyeong seok <gsr025silver.naver.com>
2025-11-03 16:43:55 +0900

    - JDBC와 JPA, Hibernate 실습을 위한 환경 세팅 - Spring Web, Spring Data JDBC, Spring Data JPA, H2 Database 의존성 추가

---
+++ learn-java-and-hibernate/src/main/java/com/backstone/springboot/learn_java_and_hibernate/LearnJavaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.backstone.springboot.learn_java_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ learn-java-and-hibernate/src/test/java/com/backstone/springboot/learn_java_and_hibernate/LearnJavaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backstone.springboot.learn_java_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
