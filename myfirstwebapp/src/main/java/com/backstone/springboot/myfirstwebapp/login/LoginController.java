a4e9bcd164e9b82995a603fc3d1496fe01f0244d
Bea gyeong seok <gsr025silver.naver.com>
2025-11-08 16:51:05 +0900

    view에 사용자 입력값을 전달하여 출력하기 - query parameter에 사용자 입력값을 넣고, 해당 값은 model을 사용해 view를 전달하는 것을 구현하였다. 	- query parameter는 url에 정보를 전달하는 방법이다. 	- model을 사용해 query parameter값을 view에서 쓸 수 있도록 할 수 있다. - @RequestParam을 사용해 controller에게 query parameter인 사용자가 입력값을 알려주도록 한다. - EL(expression language)를 사용해 model에 있는 값을 간편하게 꺼내쓸 수 있도록 한다.

--- myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,17 @@
 package com.backstone.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String gotoLoginPage(){
+    public String gotoLoginPage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
+        System.out.println("Request para is "+name); // NOT RECOMMEND FOR PROD CODE
         return "login";
     }
 }
