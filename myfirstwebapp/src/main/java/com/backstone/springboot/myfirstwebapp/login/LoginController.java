2bc350f913eaa11a12c7d7f76f0b8457fcc75615
Bea gyeong seok <gsr025silver.naver.com>
2025-11-08 17:08:42 +0900

    특정 package의 logging 수준에 따라 logging하도록 하기 - org.slf4j의 Logger와 LoggerFactory를 사용하여 구현할 수 있다.

--- myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.backstone.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model){
         model.put("name", name);
+        logger.debug("Request param is {}", name);
+        logger.info("I want this printed at info level");
+        logger.warn("I want this printed at info level");
+
         System.out.println("Request para is "+name); // NOT RECOMMEND FOR PROD CODE
+
         return "login";
     }
 }
