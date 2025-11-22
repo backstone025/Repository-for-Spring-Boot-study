abf32c6dbb1941f5d1b721466ff6c6ff0e23649d
Bea gyeong seok <gsr025silver.naver.com>
2025-11-23 00:40:42 +0900

    CSRF(Cross-Site Request Forgery) 보호 비활성화하여 h2 console에 접근할 수 있도록 구현했다. - SpringSecurity version이 올라감에 따라 기존 명령어 형태에서 람다 함수를 사용한 형식으로 설정해야 한다.

--- myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/backstone/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.backstone.springboot.myfirstwebapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -37,5 +41,20 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain fileterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+        // SpringSecurity 6.1 version부터는 해당 명령어 방식을 사용하지 않는다.
+        // http.csrf().disable();
+        // http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
