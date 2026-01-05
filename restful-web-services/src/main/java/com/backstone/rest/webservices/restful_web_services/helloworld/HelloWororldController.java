2b6c242399a75c7b227b983d8d3ca75c04e67b42
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-01-05 21:05:32 +0900

    path parameter을 사용하는 방법을 학습했다.

--- restful-web-services/src/main/java/com/backstone/rest/webservices/restful_web_services/helloworld/HelloWororldController.java
+++ restful-web-services/src/main/java/com/backstone/rest/webservices/restful_web_services/helloworld/HelloWororldController.java
@@ -1,6 +1,7 @@
 package com.backstone.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class HelloWororldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorlPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(
+                String.format("Hello World %s!", name)
+        );
     }
 }
