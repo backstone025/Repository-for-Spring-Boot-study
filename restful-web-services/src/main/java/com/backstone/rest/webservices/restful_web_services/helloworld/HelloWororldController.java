050522fa500b6a72cb1f28ad4f0ec43f56debe74
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-01-05 20:22:46 +0900

    첫 RESTful api 구현 - @RestController 사용함 - JASON 형식으로 반환하도록 구현

---
+++ restful-web-services/src/main/java/com/backstone/rest/webservices/restful_web_services/helloworld/HelloWororldController.java
@@ -0,0 +1,18 @@
+package com.backstone.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWororldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
